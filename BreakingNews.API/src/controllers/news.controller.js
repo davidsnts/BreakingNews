63f15da89cbd9bdf73568727fdc3814333c642b2
davidsnts <david.santos@delage.com.br>
2024-10-14 10:21:18 -0300

    Implmentando busca por título e notícias por usuário

--- BreakingNews.API/src/controllers/news.controller.js
+++ BreakingNews.API/src/controllers/news.controller.js
@@ -3,6 +3,9 @@ import {
   findAllService,
   findByIdService,
   countNews,
+  topNewsService,
+  searchByTitleService,
+  byUserService,
 } from "../services/news.service.js";
 
 const create = async (req, res) => {
@@ -67,9 +70,108 @@ const findAll = async (req, res) => {
         userAvatar: item.user.avatar,
       })),
     });
-  } catch (error) {
+  } catch (err) {
     return res.status(500).send({ message: err });
   }
 };
 
-export { create, findAll };
+const topNews = async (req, res) => {
+  try {
+    const news = await topNewsService();
+    if (!news)
+      return res
+        .status(400)
+        .send({ message: "Não existe uma notícia cadastrada" });
+
+    return res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        userName: news.user.username,
+        name: news.user.name,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
+
+const findById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const news = await findByIdService(id);
+    if (!news)
+      return res
+        .status(400)
+        .send({ message: "Não existe uma notícia cadastrada" });
+
+    return res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        userName: news.user.username,
+        name: news.user.name,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
+
+const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query;
+    const news = await searchByTitleService(title);
+    console.log(news);
+
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        userName: item.user.username,
+        name: item.user.name,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
+
+const byUser = async (req, res) => {
+  try {
+    const id = req.userId;
+    const news = await byUserService(id);
+
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        userName: item.user.username,
+        name: item.user.name,
+        userAvatar: item.user.avatar,
+      })),
+    });
+
+
+  } catch (err) {}
+};
+export { create, findAll, topNews, findById, searchByTitle, byUser };
--- BreakingNews.API/src/services/news.service.js
+++ BreakingNews.API/src/services/news.service.js
@@ -1,14 +1,32 @@
 import News from "../models/News.js";
 
 const createService = (body) => News.create(body);
-const findAllService = (limit, offset) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
-const findByIdService = (id) => News.findById(id);
+const findAllService = (limit, offset) =>
+  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+const findByIdService = (id) => News.findById(id).populate("user");
 const countNews = () => News.countDocuments();
+const topNewsService = () => News.findOne().sort({ _id: -1 }).populate("user");
 
+// const searchByTitleService = (title) => News.find({
+//    title: {$regex: `${title || ""} `, $options: "i"}
+// }).sort({ _id: -1 }).populate("user");
 
+const searchByTitleService = (title) =>
+  News.find({
+    title: { $regex: `.*${title}.*`, $options: "i" },
+  })
+    .sort({ _id: -1 })
+    .populate("user");
+
+const byUserService = (id) => {
+  News.find({ user: id }).sort({ _id: -1 }).populate("user");
+};
 export {
   createService,
   findAllService,
   findByIdService,
-  countNews
+  countNews,
+  topNewsService,
+  searchByTitleService,
+  byUserService,
 };
