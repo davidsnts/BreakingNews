02f8be29f6b13d3c865c4771c22fae6301a9972b
davidsnts <david.santos@delage.com.br>
2024-10-17 21:02:50 -0300

    Adicionada documentação e swagger

--- BreakingNews.API/index.js
+++ BreakingNews.API/index.js
@@ -3,7 +3,8 @@ import connectDatabase from "./src/database/db.js";
 import dotenv from "dotenv";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
-import newsRoute from "./src/routes/news.route.js"
+import swaggerRoute from "./src/routes/swagger.route.js";
+import newsRoute from "./src/routes/news.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -15,4 +16,5 @@ connectDatabase();
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
--- BreakingNews.API/src/controllers/user.controller.js
+++ BreakingNews.API/src/controllers/user.controller.js
@@ -4,16 +4,18 @@ const create = async (req, res) => {
   try {
     const { name, username, email, password, avatar, background } = req.body;
     if (!name || !username || !email || !password || !avatar || !background) {
-      res.status(400).send({ message: "submit all fields for registration" });
+      return res
+        .status(400)
+        .send({ message: "submit all fields for registration" });
     }
     const user = await userService.create(req.body);
     if (!user) res.status(400).send({ message: "Erro ao criar o usuário" });
-    res.status(201).send({
+    return res.status(201).send({
       message: "User created successfully",
       user: { id: user._id, name, username, email, avatar, background },
     });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    return res.status(500).send({ message: err.message });
   }
 };
 
@@ -21,19 +23,21 @@ const findAll = async (req, res) => {
   try {
     const users = await userService.findAllService();
     if (users.lenght === 0)
-      res.status(400).send({ message: "Não existem usuários cadastrados" });
-    res.status(200).send({ users });
+      return res
+        .status(400)
+        .send({ message: "Não existem usuários cadastrados" });
+    return res.status(200).send({ users });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    return res.status(500).send({ message: err.message });
   }
 };
 
 const findById = async (req, res) => {
   try {
     const user = req.user;
-    res.status(200).send({ user });
+    return res.status(200).send({ user });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    return res.status(500).send({ message: err.message });
   }
 };
 
@@ -42,7 +46,9 @@ const update = async (req, res) => {
     const { user, id } = req;
     const { name, username, email, password, avatar, background } = req.body;
     if (!name && !username && !email && !password && !avatar && !background) {
-      res.status(400).send({ message: "submit at least one field for update" });
+      return res
+        .status(400)
+        .send({ message: "submit at least one field for update" });
     }
     await userService.updateService(
       id,
@@ -53,9 +59,9 @@ const update = async (req, res) => {
       avatar,
       background
     );
-    res.status(200).send({ message: "Usuário atualizado com sucesso" });
+    return res.status(200).send({ message: "Usuário atualizado com sucesso" });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    return res.status(500).send({ message: err.message });
   }
 };
 
